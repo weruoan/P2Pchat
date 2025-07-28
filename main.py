4b0112ef46a39bddfb6b108e8dcbd3ee1f4fc154
stanislav200 <stanislav200@stasyan.stasyan>
2025-07-28 13:31:51 +0300

    добавлены потоки

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
+from os import access
 import socket
 import threading
-import pickle
+import time
+
 from server import Server
 
-server = Server('127.0.0.1', 12345)
-print(server)
-server.send_discover()
+def send_discover(timeout: int, server: Server):
+    while True:
+        server.send_discover()
+        time.sleep(timeout)
+        
+def accept_discover(timeout: int, server: Server):
+    while True:
+        server.accept_discover()
+        time.sleep(timeout)
+    
+
+def chat():
+    server = Server('127.0.0.1', 12345)
+    send_signal = threading.Thread(target=send_discover, args=(5,server))
+    accept_signal = threading.Thread(target=accept_discover, args=(1, server))
+    send_signal.daemon = True
+    accept_signal.daemon = True
+    try:
+        send_signal.start()
+        accept_signal.start()
+        send_signal.join()
+        accept_signal.join()
+    except KeyboardInterrupt:
+        print('Остановка программы')
+    
+    
+    
+
+if __name__ == "__main__":
+    
+    chat()
--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
+from operator import add
 import socket
-import threading
+from time import sleep
+from queue import Queue
 
  # Коды ответов, запросов:
      # \x01 -- hello
@@ -11,6 +13,8 @@ class Server:
     def __init__(self, host='127.0.0.1', port=23553, max_users=5) -> None:
         self.host = host
         self.port = port
+        self.addr = None
+        self.data_queue = Queue()
         self.connections = set()
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -19,20 +23,17 @@ class Server:
         self.udp_socket.bind((host, port))
         self.tcp_socket.listen(max_users)
 
-    def send_discover(self):
+    def send_discover(self, timeout=5):
         print('sending discover')
         self.udp_socket.sendto(b'\x01', ('255.255.255.255', self.port))
 
     def accept_discover(self):
         print('accepting discover')
         addr, data = self.udp_socket.recvfrom(1024)
-        if data == b'\x01':
-            self.udp_socket.sendto(b'\x02', addr)
-            self.connections.add(addr)
-            addr, data = self.udp_socket.recvfrom(1024)
-            if data != b'\x03':
-                self.connections.pop(addr)
-
-        elif data == b'\x02':
-            self.connections.add(addr)
-            self.udp_socket.sendto(b'\x03', addr)
+        self.data_queue.put({addr: data})
+        
+    def add_member(self):
+        print(self.data_queue)
+        
+    def show_members(self):
+        print(self.connections)
