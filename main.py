5acdafa91536d07fca035d2fd86bd09f39cabedb
stanislav200 <stanislav200@stasyan.stasyan>
2025-07-29 16:15:40 +0300

    Улучшена структура класса server перенесены поток в класс, создан специальный метод start для запуска

--- main.py
+++ main.py
@@ -1,37 +1,11 @@
-from os import access
-import socket
-import threading
-import time
-
 from server import Server
 
 
-def send_discover(timeout: int, server: Server):
-    while True:
-        server.send_discover()
-        time.sleep(timeout)
-
-
-def accept_discover(timeout: int, server: Server):
-    while True:
-        server.accept_discover()
-        time.sleep(timeout)
-
 
 def chat():
-    server = Server('0.0.0.0', 12345)
-    send_signal = threading.Thread(target=send_discover, args=(5, server))
-    accept_signal = threading.Thread(target=accept_discover, args=(1, server))
-    distributor = threading.Thread(target=server.distributor)
-    send_signal.daemon = True
-    accept_signal.daemon = True
     try:
-        send_signal.start()
-        accept_signal.start()
-        distributor.start()
-        send_signal.join()
-        accept_signal.join()
-        distributor.join()
+        serverok = Server()
+        serverok.start()
     except KeyboardInterrupt:
         print('Остановка программы')
 
--- server/server.py
+++ server/server.py
@@ -1,10 +1,11 @@
-from operator import add
 import socket
-from time import sleep
 from queue import Queue
 from typing import Dict
+from server.chat import Chat
+from threading import Thread
+import time
 
-
+# Chat()
 # Коды ответов, запросов:
 # \x01 -- hello
 # \x02 -- accept
@@ -16,40 +17,56 @@ from typing import Dict
 # 1-ый байт -- код ответа (запроса)
 
 class Server:
-
-    def __init__(self, host='127.0.0.1', port=23553, max_users=5) -> None:
+    def __init__(self, host='0.0.0.0', tcp_port=23553, udp_port=23554, max_users=5) -> None:
         self.host = host
-        self.port = port
+        self.tcp_port = tcp_port
+        self.udp_port = udp_port
         self.addr = None
         self.data_queue: Queue[Dict[tuple[str, int], bytes]] = Queue()
         self.connections: set[tuple[str, int]] = set()
+        self.waiting_for_accept: set[tuple[str, int]] = set()
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.tcp_socket.bind((host, port))
-        self.udp_socket.bind((host, port))
+        self.tcp_socket.bind((host, tcp_port))
+        self.udp_socket.bind((host, udp_port))
         self.tcp_socket.listen(max_users)
-
+        
+    
     def send_discover(self, timeout=5):
-        print('sending discover')
-        self.udp_socket.sendto(b'\x01', ('255.255.255.255', self.port))
+        while True:
+            print('sending discover')
+            self.udp_socket.sendto(b'\x01', ('255.255.255.255', self.udp_port))
+            time.sleep(timeout)
+        
+        
 
-    def accept_discover(self):
-        print('accepting discover')
-        data, addr = self.udp_socket.recvfrom(1024)
-        self.data_queue.put({addr: data})
-
+    def accept_discover(self, timeout=5):
+        while True:
+            print('accepting discover')
+            data, addr = self.udp_socket.recvfrom(1024)
+            self.data_queue.put({addr: data})
+            time.sleep(timeout)
+        
+    def create_connect(self, addr):
+        self.waiting_for_accept.add(addr)
+        self.udp_socket.sendto(b'\x02', addr)
+        
+        
     def distributor(self):
         while True:
             packed_data = self.data_queue.get()
             addr, data = next(iter(packed_data.items()))
             print('data:', data, addr)
             if data is None:
-                print(2)
                 continue
             elif data == b'\x01':
-                
-                self.add_member(addr)
+                self.udp_socket.sendto(b'\x01', ('255.255.255.255', self.udp_port))
+            elif data == b'\x03':
+                self.waiting_for_accept.remove(addr)
+                self.connections.add(addr)
+                print('members:', self.connections)
+            
 
     def add_member(self, member):
         self.connections.add(member)
@@ -57,3 +74,16 @@ class Server:
 
     def show_members(self):
         print(self.connections)
+        
+    def start(self):
+        send_signal = Thread(target=self.send_discover, args=(5,), daemon=True)
+        accept_signal = Thread(target=self.accept_discover, args=(1,), daemon=True)
+        distributor = Thread(target=self.distributor, daemon=True)
+        send_signal.start()
+        accept_signal.start()
+        distributor.start()
+        send_signal.join()
+        accept_signal.join()
+        distributor.join()
+        
+        
